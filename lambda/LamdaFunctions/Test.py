4e9864c29d48773b46793e2c7674d2ffb8d422be
Rao Gottapu <achyuta.rao.gottapu@accenture.com>
2020-07-10 19:08:38 +0530

    test

--- lambda/LamdaFunctions/Test.py
+++ lambda/LamdaFunctions/Test.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 ec2 = boto3.client('ec2')
-# It is my first programme
+# It is my first programme and exc
 def lambda_handler(event, context):
     response = ec2.describe.availability.zones()
     return{"statuscode":200, "body":json.dumps(response)}   
